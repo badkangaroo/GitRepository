78d80d4e5a4b1f26b631482070c78c05e7a2b47f
alexokita <alex.okita@gmail.com>
2013-08-18 21:48:28 -0700

    removing orig

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,8 +3,5 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
 	public int AprilsNumber;
-<<<<<<< HEAD
 	public int AlexsNumber;
-=======
->>>>>>> 15139199ae4ac630e3d1e47e0cb006ae29f3e52a
 }
