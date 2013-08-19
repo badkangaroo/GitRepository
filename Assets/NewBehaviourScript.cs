2d7b8063a1a815f6216d12d13b24714e35b64a7b
alexokita <alex.okita@gmail.com>
2013-08-18 21:13:15 -0700

    fixing up after merge

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
 	public int somenumber;
+	public int othernumber;
 	// Use this for initialization
 	void Start () {
 	
