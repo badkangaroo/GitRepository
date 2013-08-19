d4c1639d57d4ba009c66b615562aabd9b8719569
aprilaprilapril <emailingapril@gmail.com>
2013-08-18 21:18:01 -0700

    making things short

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,13 +3,4 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
 	public int aprilsNumber;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
