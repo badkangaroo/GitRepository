bfa0b3b70214e8faa8f6992f9be303287e857f00
aprilaprilapril <emailingapril@gmail.com>
2013-08-18 21:15:23 -0700

    making aprils num

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
-	public int somenumber;
-	public int othernumber;
+	public int aprilsNumber;
 	// Use this for initialization
 	void Start () {
 	
