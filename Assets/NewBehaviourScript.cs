dab251229afe9d75a778e34996bfe9e3272a1ebd
alexokita <alex.okita@gmail.com>
2013-08-18 21:19:10 -0700

    changing?

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,15 +2,5 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
-	public int somenumber;
-	public int othernumber;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public int AprilsNumber;
 }
